ac1d4e44aead8ad998014af91eedc0b7e0a536db
arabomeivan <arabomeivan@protonmail.com>
2025-04-06 20:13:29 +0100

    lkl

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,30 +1,33 @@
 <template>
+  <div>
     <nav :class="pageScrolledY ? 'navbar navbar-expand-lg navbar-light bg-light fixed-top':'navbar navbar-expand-lg navbar-light bg-light bg-lg-transparent fixed-top'">
-        <div class="container">
-            <div class="d-flex justify-content-end w-100 d-lg-none">
-                <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"/>
-                  </button>
-            </div>
-          
-          <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"/>
-          </button> -->
-          <div id="navbarTogglerDemo01" class="collapse navbar-collapse text-center d-lg-flex justify-content-lg-between">
-            <NuxtLink to="/" class="text-primary text-decoration-none"><b class="fs-2">Zort</b></NuxtLink>
-            <ul class="navbar-nav mb-2 mb-lg-0 ">
-              <li v-for="link in navLinks" :key="link.id" class="nav-item">
-                <NuxtLink :to="link.path" :class=" isActive===link.path? 'nav-link active text-secondary':'nav-link text-secondary' " aria-current="page" href="#"><b>{{ link.label }}</b></NuxtLink>
-              </li>
-            </ul>
-            <div class="d-lg-flex">
-
-                <NuxtLink to="/signup"><button class="btn text-white border-0" type="submit">Sign Up</button></NuxtLink>
-            </div>
-
+      <div class="container">
+          <div class="d-flex justify-content-end w-100 d-lg-none">
+              <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"/>
+                </button>
           </div>
+        
+        <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"/>
+        </button> -->
+        <div id="navbarTogglerDemo01" class="collapse navbar-collapse text-center d-lg-flex justify-content-lg-between">
+          <NuxtLink to="/" class="text-primary text-decoration-none"><b class="fs-2">Zort</b></NuxtLink>
+          <ul class="navbar-nav mb-2 mb-lg-0 ">
+            <li v-for="link in navLinks" :key="link.id" class="nav-item">
+              <NuxtLink :to="link.path" :class=" isActive===link.path? 'nav-link active text-secondary':'nav-link text-secondary' " aria-current="page" href="#"><b>{{ link.label }}</b></NuxtLink>
+            </li>
+          </ul>
+          <div class="d-lg-flex">
+
+              <NuxtLink to="/signup"><button class="btn text-white border-0" type="submit">Sign Up</button></NuxtLink>
+          </div>
+
         </div>
-      </nav>
+      </div>
+    </nav>
+  </div>
+    
 </template>
 <script>
 export default {
